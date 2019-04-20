2e55ea6453f4c30ee4683d15bbc0fcaf56eb3246
JoaquinBarrientos <37567734+JoaquinBarrientos@users.noreply.github.com>
2019-04-20 13:50:57 -0600

    corrección error en el nav bar
    Habi puesto mal la ruta de los archivos Js

--- html/principal.php
+++ html/principal.php
@@ -58,9 +58,9 @@
         </div>
     </div>
     <!-- Scripts JS-->
-    <script src="js/jquery-3.2.1.slim.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/jquery-3.2.1.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </body>
 
 </html>
